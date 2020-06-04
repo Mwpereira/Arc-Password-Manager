a93803c52b1d94ef8cc76976dab8e2dd9b625365
Michael Pereira <michael.w.pereira.7@gmail.com>
2020-06-04 12:21:53 -0400

    Updated Settings UI

--- arc-password-manager/client/src/views/Login.vue
+++ arc-password-manager/client/src/views/Login.vue
@@ -1,96 +1,103 @@
 <template>
-  <div class="login">
-    <h1>Arc</h1>
-    <form @submit.prevent="authorizeAccount" class="validation-form">
-      <input
-        autofocus
-        class="input-bar"
-        type="text"
-        placeholder="Username"
-        v-model="username"
-        required
-      />
-      <input class="input-bar" type="password" placeholder="Password" v-model="password" required />
-      <input class="action-btn" type="submit" value="Login" />
-      <input
-        class="action-btn"
-        id="loginArc-btn"
-        value="Need to create an account? Sign up"
-        onclick="javascript:location.href='/signup'"
-      />
-      <p
-        style="background-color: red;"
-        class="login-error-message"
-        v-if="errorMessage != ''"
-      >{{ errorMessage }}</p>
-    </form>
-  </div>
+    <div class="login">
+        <h1>Arc</h1>
+        <form @submit.prevent="authorizeAccount" class="validation-form">
+            <input
+                autofocus
+                class="input-bar"
+                type="text"
+                placeholder="Username"
+                v-model="username"
+                required
+            />
+            <input
+                class="input-bar"
+                type="password"
+                placeholder="Password"
+                v-model="password"
+                required
+            />
+            <input class="action-btn" type="submit" value="Login" />
+            <input
+                class="action-btn"
+                id="loginArc-btn"
+                value="Need to create an account? Sign up"
+                onclick="javascript:location.href='/signup'"
+            />
+            <p class="login-error-message" v-if="errorMessage != ''">
+                {{ errorMessage }}
+            </p>
+        </form>
+    </div>
 </template>
 
 <script>
 import bcrypt from "bcryptjs";
 
 export default {
-  name: "Login",
-  data: () => {
-    return {
-      username: '',
-      password: '',
-      errorMessage: ''
-    };
-  },
-  methods: {
-    async authorizeAccount() {
-      let form = document.getElementsByTagName("form")[0];
+    name: "Login",
+    data: () => {
+        return {
+            username: "",
+            password: "",
+            errorMessage: "",
+        };
+    },
+    methods: {
+        async authorizeAccount() {
+            let form = document.getElementsByTagName("form")[0];
 
-      let username = form.children[0].value;
-      let password = form.children[1].value;
+            let username = form.children[0].value;
+            let password = form.children[1].value;
 
-      if (username != "" && password != "") {
-        try {
-          if (localStorage.getItem(username) != null) {
-            if (
-              await bcrypt.compare(password, localStorage.getItem(username))
-            ) {
-              if (localStorage.getItem("adEnabled") == "false") {
-                window.location.href = "/main/home";
-              } else {
-                window.location.href = "/newUser";
-              }
-            } else {
-              this.errorMessage = "Username or Password Is Incorrect";
+            if (username != "" && password != "") {
+                try {
+                    if (localStorage.getItem(username) != null) {
+                        if (await bcrypt.compare(password, localStorage.getItem(username))) {
+                            if (localStorage.getItem("adEnabled") == "false") {
+                                window.location.href = "/main/home";
+                            } else {
+                                window.location.href = "/newUser";
+                            }
+                        } else {
+                            this.errorMessage = "Username or Password Is Incorrect";
+                        }
+                    } else {
+                        this.errorMessage = "Username or Password Is Incorrect";
+                    }
+                } catch {
+                    this.errorMessage = "Username or Password Is Incorrect";
+                }
             }
-          } else {
-            this.errorMessage = "Username or Password Is Incorrect";
-          }
-        } catch {
-          this.errorMessage = "Username or Password Is Incorrect";
-        }
-      }
-    }
-  }
+        },
+    },
 };
 </script>
 
 <style scoped>
 div {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 
-  background-image: url("../assets/background900.jpg");
-  height: 100%;
+    background-image: url("../assets/background900.jpg");
+    height: 100%;
 }
 
 p {
-  display: none;
+    font-weight: bold;
+    font-size: 20px;
+    font-style: italic;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+    color: red;
 }
 
 @media (min-width: 900px), (min-height: 700px) {
-  div {
-    background-image: url("../assets/background1920.jpg");
-  }
+    div {
+        background-image: url("../assets/background1920.jpg");
+    }
 }
 </style>
--- arc-password-manager/client/src/views/Settings.vue
+++ arc-password-manager/client/src/views/Settings.vue
@@ -1,185 +1,254 @@
 <template>
-  <div class="settings">
-    <Toggle :mode="mode" @toggle="$emit('toggle')" />
-    <form @submit.prevent="saveSettings">
-      <span class="salt">
-        <h3 id="saltHeader">Salt Level:</h3>
-        <input class="input-bar" id="salt" type="number" min="1" max="20" v-model="saltLevel" />
-      </span>
-      <p style="margin-left: 3rem; margin-top: 1rem;">
-        - Scale: 1-20
-        <br />- Default/Recommended: 10
-        <br />- Higher Salt Level = Greater Security & Longer Encryption Time
-      </p>
-      <span class="arc-ad">
-        <h3 id="adHeader">Arc Startup Ad:</h3>
-        <input type="checkbox" id="cbAd" value="Enabled" v-model="ckAdEnabled" />
-        <label for="cbAd" id="lblAd">Enabled</label>
-        <br />
-        <br />
-      </span>
-      <span class="darkmode">
-            <h3 id="themeHeader">Dark Mode:</h3>
+    <div class="settings">
+        <Toggle :mode="mode" @toggle="$emit('toggle')" />
+        <form @submit.prevent="saveSettings">
+            <span class="salt">
+                <h3 id="saltHeader">Salt Level:</h3>
+                <div @submit.prevent="updateSalt" class="slidecontainer">
+                    <input
+                        type="range"
+                        min="1"
+                        max="20"
+                        class="slider"
+                        id="saltRange"
+                        v-on:click="updateSalt"
+                    />
+                </div>
+                <p type="number" id="salt">Null</p>
+            </span>
+            <p style="margin-left: 3rem; margin-top: 1rem;">
+                - Scale: 1-20
+                <br />- Default/Recommended: 10 <br />- Higher Salt Level = Greater Security &
+                Longer Encryption Time
+            </p>
+            <span class="arc-ad">
+                <h3 id="adHeader">Arc Startup Ad:</h3>
+                <input type="checkbox" id="cbAd" value="Enabled" v-model="ckAdEnabled" />
+                <label for="cbAd" id="lblAd">Enabled</label>
+                <br />
+                <br />
+            </span>
+            <span class="darkmode">
+                <h3 id="themeHeader">Dark Mode:</h3>
 
-            <div class="toggle-wrapper">
-                <label class="toggle">
-            <input type="checkbox" id="cbDarkMode" value="Enabled" v-model="cbDarkMode" :checked="(mode === 'dark') ? 'checked' : false" 
-            @change="$emit('toggle')" />
-            <span class="toggler round"></span>
-                </label>
-            </div>
-      </span>
+                <div class="toggle-wrapper">
+                    <label class="toggle">
+                        <input
+                            type="checkbox"
+                            id="cbDarkMode"
+                            value="Enabled"
+                            v-model="cbDarkMode"
+                            :checked="mode === 'dark' ? 'checked' : false"
+                            @change="$emit('toggle')"
+                        />
+                        <span class="toggler round"></span>
+                    </label>
+                </div>
+            </span>
 
-      <input class="main-btn" id="save" type="submit" value="Save" />
-    </form>
-  </div>
+            <input class="main-btn" id="save" type="submit" value="Save" />
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "Settings",
-  props: ['mode'],
-  data: () => {
-    return {
-      salt: 10,
-      adEnabled: "",
-      darkMode: ""
-    };
-  },
-  methods: {
-    loadSettings() {
-      let salt = localStorage.getItem("salt");
-      let adEnabled = localStorage.getItem("adEnabled");
-      let darkMode = localStorage.getItem("darkMode");
-
-      let saltValue = document.getElementById("salt");
-      let checkedValue = document.getElementById("cbAd");
-      let darkModeValue = document.getElementById("cbDarkMode");
-
-      saltValue.value = salt;
-      this.saltLevel = salt;
-
-      if (adEnabled == "true") {
-        checkedValue.checked = true;
-        this.checkedValue = true;
-      } else {
-        checkedValue.checked = false;
-        this.checkedValue = false;
-      }
-
-      if (darkMode == "true") {
-        darkModeValue.checked = true;
-        this.darkMode = true;
-      } else {
-        darkModeValue.checked = false;
-        this.darkMode = false;
-      }
+    name: "Settings",
+    props: ["mode"],
+    data: () => {
+        return {
+            saltLevel: 10,
+            adEnabled: "",
+            darkMode: "",
+        };
     },
-    saveSettings() {
-      let salt = this.saltLevel;
-      let adEnabled = this.ckAdEnabled;
+    methods: {
+        loadSettings() {
+            var slider = document.getElementById("saltRange");
+            var output = document.getElementById("salt");
 
-      localStorage.setItem("salt", salt);
+            let salt = localStorage.getItem("salt");
+            let adEnabled = localStorage.getItem("adEnabled");
+            let darkMode = localStorage.getItem("darkMode");
 
-      if(adEnabled != undefined){
-        localStorage.setItem("adEnabled", adEnabled);
-      }
-    }
-  },
-  mounted() {
-    this.loadSettings();
-  }
+            let saltValue = document.getElementById("salt");
+            let checkedValue = document.getElementById("cbAd");
+            let darkModeValue = document.getElementById("cbDarkMode");
+
+            saltValue.value = salt;
+            this.saltLevel = salt;
+            slider.value = salt;
+
+            output.innerHTML = slider.value; // Display the default slider value
+
+            if (adEnabled == "true") {
+                checkedValue.checked = true;
+                this.checkedValue = true;
+            } else {
+                checkedValue.checked = false;
+                this.checkedValue = false;
+            }
+
+            if (darkMode == "true") {
+                darkModeValue.checked = true;
+                this.darkMode = true;
+            } else {
+                darkModeValue.checked = false;
+                this.darkMode = false;
+            }
+        },
+        saveSettings() {
+            let salt = this.saltLevel;
+            let adEnabled = this.ckAdEnabled;
+
+            if (adEnabled != undefined) {
+                localStorage.setItem("salt", salt);
+            }
+
+            if (adEnabled != undefined) {
+                localStorage.setItem("adEnabled", adEnabled);
+            }
+        },
+        updateSalt() {
+            var slider = document.getElementById("saltRange");
+            var output = document.getElementById("salt");
+
+            slider.oninput = function() {
+                output.innerHTML = this.value;
+                this.saltLevel = output.innerHTML;
+            };
+        },
+    },
+    mounted() {
+        this.loadSettings();
+    },
 };
 </script>
 
 <style scoped>
 div {
-  overflow: hidden;
-  height: 90%;
+    overflow: hidden;
+    height: 90%;
 }
 
 label {
-  font-size: 20px;
+    font-size: 20px;
 }
 
 p {
-  font-size: 18px;
-  cursor: default;
+    font-size: 18px;
+    cursor: default;
 }
 
 .salt {
-  display: flex;
-  flex-direction: row;
+    display: flex;
+    flex-direction: row;
 }
 
 .arc-ad {
-  display: flex;
-  flex-direction: row;
+    display: flex;
+    flex-direction: row;
 }
 
-.darkmode{
-  display: flex;
-  flex-direction: row;
+.darkmode {
+    display: flex;
+    flex-direction: row;
 }
 
 #saltHeader {
-  font-size: 22px;
-  margin-top: 3rem;
-  margin-left: 3rem;
-  cursor: default;
+    font-size: 22px;
+    margin-top: 3rem;
+    margin-left: 3rem;
+    cursor: default;
 }
 
 #salt {
-  align-self: center;
-  width: 3rem;
-  margin-top: 2.5rem;
-  margin-left: 3rem;
-  color: black;
-  opacity: 1
+    align-self: center;
+    width: 3rem;
+    margin-top: 2.5rem;
+    margin-left: 3rem;
+    color: black;
+    opacity: 1;
 }
 
 #save {
-  float: right;
-  margin: 5rem;
+    float: right;
+    margin: 5rem;
 }
 
 #adHeader {
-  font-size: 22px;
-  margin-top: 3rem;
-  margin-left: 3rem;
-  cursor: default;
+    font-size: 22px;
+    margin-top: 3rem;
+    margin-left: 3rem;
+    cursor: default;
 }
 
 #cbAd {
-  cursor: pointer;
-  justify-self: center;
-  align-self: center;
-  margin-left: 2rem;
-  margin-top: 3.25rem;
+    cursor: pointer;
+    justify-self: center;
+    align-self: center;
+    margin-left: 2rem;
+    margin-top: 3.25rem;
 }
 
 #lblAd {
-  margin-left: 1rem;
-  margin-bottom: 0.05rem;
-  align-self: flex-end;
+    margin-left: 1rem;
+    margin-bottom: 0.05rem;
+    align-self: flex-end;
 }
 
 #themeHeader {
-  font-size: 22px;
-  margin-top: 3rem;
-  margin-left: 3rem;
-  cursor: default;
+    font-size: 22px;
+    margin-top: 3rem;
+    margin-left: 3rem;
+    cursor: default;
 }
 
-#cbDarkMode{
-  cursor: pointer;
-  justify-self: center;
-  align-self: center;
-  margin-left: 2rem;
-  margin-top: 5rem;
+#cbDarkMode {
+    cursor: pointer;
+    justify-self: center;
+    align-self: center;
+    margin-left: 2rem;
+    margin-top: 5rem;
 }
 
-.toggle{
+.slidecontainer {
+    width: 25%;
+}
+
+.slider {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 100%;
+    height: 25px;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: 0.25s;
+    transition: opacity 0.25s;
+}
+
+.slider:hover {
+    opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    background: blue;
+    cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    background: blue;
+    cursor: pointer;
+}
+
+.toggle {
     position: relative;
     display: inline-block;
     width: 60px;
@@ -188,13 +257,13 @@ p {
     margin-left: 2rem;
 }
 
-.toggle input{
+.toggle input {
     opacity: 0;
     width: 0;
     height: 0;
 }
 
-.toggler{
+.toggler {
     position: absolute;
     cursor: pointer;
     top: 0;
@@ -206,7 +275,7 @@ p {
     transition: 0.4s;
 }
 
-.toggler:before{
+.toggler:before {
     position: absolute;
     content: "";
     height: 26px;
@@ -218,51 +287,50 @@ p {
     transition: 0.4s;
 }
 
-input:checked + .toggler{
-    background: #15202B;
+input:checked + .toggler {
+    background: #15202b;
 }
 
-input:focus + toggler{
-    box-shadow: 0 0 2px #15202B;
+input:focus + toggler {
+    box-shadow: 0 0 2px #15202b;
 }
 
-input:checked + .toggler:before{
+input:checked + .toggler:before {
     -webkit-transform: translateX(26px);
     -ms-transform: translateX(26px);
     transform: translateX(26px);
 }
 
-.toggler.round{
+.toggler.round {
     border-radius: 34px;
 }
 
-.toggler.round:before{
+.toggler.round:before {
     border-radius: 50%;
 }
 
-
 .dark header {
-  background: #15202B;
+    background: #15202b;
 }
 
 header h1 {
-  font-size: 32px;
-  text-transform: uppercase;
-  font-weight: 900;
+    font-size: 32px;
+    text-transform: uppercase;
+    font-weight: 900;
 }
 header nav {
-  display: flex;
-  height: 60px;
+    display: flex;
+    height: 60px;
 }
 header nav .nav-link {
-  display: flex;
-  align-items: center;
-  padding: 0px 15px;
+    display: flex;
+    align-items: center;
+    padding: 0px 15px;
 }
 header nav .nav-link:hover {
-  background: #B0B0B0;
+    background: #b0b0b0;
 }
 .dark header nav .nav-link:hover {
-  background: #101520;
+    background: #101520;
 }
 </style>
